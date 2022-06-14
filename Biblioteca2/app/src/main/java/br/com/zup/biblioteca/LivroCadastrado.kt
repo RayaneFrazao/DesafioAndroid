763e82e3e20c35689751e4563dcecf788fa9bcc7
RayaneFrazao <rayanefrazao37@gmail.com>
2022-06-14 17:54:51 -0300

    Android

---
+++ Biblioteca2/app/src/main/java/br/com/zup/biblioteca/LivroCadastrado.kt
@@ -0,0 +1,11 @@
+package br.com.zup.biblioteca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LivroCadastrado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_livro_cadastrado)
+    }
+}
